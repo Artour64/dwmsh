d6eb91b5a7e33c4a775cf6860dda9a0e6a3467eb
Artour Konstantinov <artoukons@gmail.com>
2020-07-17 11:07:27 -0400

    moved dwm specific scripts from mysh directory into here

---
+++ exitdwm.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+#i="poweroff,reboot,exit --user,suspend,hibernate,hybrid-sleep"
+i="suspend,logout,restart,poweroff,reboot"
+i=$(echo -e $i | rofi -i -sep ',' -dmenu -p systemctl)
+if [ "$i" == "logout" ]; then
+	i="exit --user"
+elif [ "$i" == "restart" ]; then
+	#kill $(pidof dwm)
+	~/dwmsh/restartdwm.sh
+	exit
+fi
+systemctl -q $i
