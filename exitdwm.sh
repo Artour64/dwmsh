eae255690e284bf0a9c8c81de57beba749f13ca1
Artour Konstantinov <artoukons@gmail.com>
2020-08-19 17:52:00 -0400

    added screen off to dwmexit.sh renamed 'restart' to 'restart-dwm'

--- exitdwm.sh
+++ exitdwm.sh
@@ -1,12 +1,15 @@
 #/bin/bash
 #i="poweroff,reboot,exit --user,suspend,hibernate,hybrid-sleep"
-i="suspend,logout,restart,poweroff,reboot"
+i="suspend,logout,restart-dwm,screen-off,poweroff,reboot"
 i=$(echo -e $i | rofi -i -sep ',' -dmenu -p systemctl)
 if [ "$i" == "logout" ]; then
 	i="exit --user"
-elif [ "$i" == "restart" ]; then
+elif [ "$i" == "restart-dwm" ]; then
 	#kill $(pidof dwm)
 	~/dwmsh/restartdwm.sh
+	exit
+elif [ "$i" == "screen-off" ]; then
+	~/mysh/mon-off.sh
 	exit
 fi
 systemctl -q $i
