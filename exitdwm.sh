a44d5d1363ae89f9ce3a31735edf5990d6c27c38
Artour Konstantinov <artoukons@gmail.com>
2020-08-22 16:22:29 -0400

    exitdwm.sh: changed option order

--- exitdwm.sh
+++ exitdwm.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 #i="poweroff,reboot,exit --user,suspend,hibernate,hybrid-sleep"
-i="suspend,logout,restart-dwm,screen-off,poweroff,reboot"
+i="suspend,screen-off,logout,poweroff,reboot,restart-dwm"
 i=$(echo -e $i | rofi -i -sep ',' -dmenu -p systemctl)
 if [ "$i" == "logout" ]; then
 	i="exit --user"
