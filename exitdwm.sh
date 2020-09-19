23182a683ce2a9803ac24099fb1434dab64c0cff
Artour Konstantinov <artoukons@gmail.com>
2020-09-19 13:18:29 -0400

    modified:   exitdwm.sh

--- exitdwm.sh
+++ exitdwm.sh
@@ -2,7 +2,9 @@
 #i="poweroff,reboot,exit --user,suspend,hibernate,hybrid-sleep"
 i="suspend,screen-off,logout,poweroff,reboot,restart-dwm"
 i=$(echo -e $i | rofi -i -sep ',' -dmenu -p systemctl)
-if [ "$i" == "logout" ]; then
+if [ -z "$i" ]; then
+	exit
+elif [ "$i" == "logout" ]; then
 	i="exit --user"
 elif [ "$i" == "restart-dwm" ]; then
 	#kill $(pidof dwm)
