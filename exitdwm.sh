dc0f0c04d8abb1fcddb2c6ceb7fd597b1eeac4fa
Artour Konstantinov <artoukons@gmail.com>
2021-01-14 15:51:59 -0500

    modified:   exitdwm.sh

--- exitdwm.sh
+++ exitdwm.sh
@@ -1,24 +1,24 @@
-#/bin/bash
+#!/bin/bash
 #i="poweroff,reboot,exit --user,suspend,hibernate,hybrid-sleep"
 i="screen-off,suspend,logout,poweroff,reboot,restart-dwm"
 
-let "l=$(echo $i | tr ',' '\n' | wc -l)"
-if [ $( expr $l % 2) = 1 ]; then
+let "l=$(echo "$i" | tr ',' '\n' | wc -l)"
+if [ "$( expr $l % 2)" = 1 ]; then
 	let l++
 fi
 let l=l/2
 let s=240/$l
 
-i=$(echo -e $i | rofi -font "Red October $s" -lines $l -i -sep ',' -dmenu -p systemctl)
+i=$(echo "$i" | rofi -font "Red October $s" -lines $l -i -sep ',' -dmenu -p systemctl)
 if [ -z "$i" ]; then
 	exit
-elif [ "$i" == "logout" ]; then
+elif [ "$i" = "logout" ]; then
 	i="exit --user"
-elif [ "$i" == "restart-dwm" ]; then
+elif [ "$i" = "restart-dwm" ]; then
 	#kill $(pidof dwm)
 	~/dwmsh/restartdwm.sh
 	exit
-elif [ "$i" == "screen-off" ]; then
+elif [ "$i" = "screen-off" ]; then
 	~/mysh/mon-off.sh
 	exit
 fi
